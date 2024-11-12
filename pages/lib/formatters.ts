c83d384b97dc7d46fb045e908142ff9f1989f9cd
whooshinglander <whooshinglander@gmail.com>
2024-11-12 14:49:40 +0800

    Delete pages directory

--- pages/lib/formatters.ts
+++
@@ -1,5 +0,0 @@
-// lib/formatters.ts
-export function formatNumber(value: number): string {
-    return new Intl.NumberFormat('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(value);
-  }
-  
